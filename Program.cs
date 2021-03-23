c30468a209dd4de87612e8ab131bcd91f74f3cfb
Viacheslav <viacheslav.kisliy@gmail.com>
2021-03-23 15:18:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GAMPExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
